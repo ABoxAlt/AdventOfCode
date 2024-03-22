cf81363b7ad82a0b9ffe70fc39362fa64318355c
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-03-22 08:59:15 -0700

    Added comments to Day 1 Part 2

--- 2015/Day 1/Part 2/code.js
+++ 2015/Day 1/Part 2/code.js
@@ -1,12 +1,17 @@
+// Importing the input data
 const fs = require("fs");
 const chars = fs.readFileSync("./2015/Day 1/input.txt").toString();
+// Declaring vars
 let floor = 0;
+// Using i to tell which index the first basement floor is
 for (let i = 0; i < chars.length; i ++) {
+  // Checking if Santa needs to go up or down
   if (chars[i] == "(") {
     floor ++;
   } else {
     floor --;
   }
+  // If Santa enters the basement log the answer and end
   if (floor < 0) {
     console.log(i + 1);
     break;
