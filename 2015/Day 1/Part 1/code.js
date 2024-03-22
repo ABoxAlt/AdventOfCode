3e7d8010946691caab8644d0f47787fab76b4024
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-03-22 08:57:15 -0700

    Added comments to Day 1 Part 1

--- 2015/Day 1/Part 1/code.js
+++ 2015/Day 1/Part 1/code.js
@@ -1,11 +1,16 @@
+// Importing the input data
 const fs = require("fs");
 const chars = fs.readFileSync("./2015/Day 1/input.txt").toString();
+// Declaring vars
 let floor = 0;
+// Looping over each instruction
 for (const char of chars) {
+  // Checking if the instruction calls for up 1 floor or down 1 floor
   if (char == "(") {
     floor ++;
   } else {
     floor --;
   }
 }
+// print the answer
 console.log(floor);
