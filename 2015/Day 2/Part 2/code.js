959c04a0bfe08c18be12521337ee7b401dd46dde
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-03-24 14:28:50 -0700

    Added Day 2

--- 2015/Day 2/Part 2/code.js
+++ 2015/Day 2/Part 2/code.js
@@ -3,6 +3,7 @@ const lines = fs.readFileSync("./2015/Day 2/input.txt").toString().split("\n");
 let total = 0;
 for (const line of lines) {
   const [l, w, h] = line.split("x");
-  total += Math.min(l * w, l * h, h * w) + (2 * (l * w + l * h + h * w));
+  // make these ints
+  total += Math.min((2 * (parseInt(l) + parseInt(w))), (2 * (parseInt(l) + parseInt(h))), (2 * (parseInt(h) + parseInt(w)))) + (l * w * h);
 }
 console.log(total);
