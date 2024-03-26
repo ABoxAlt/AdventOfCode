2f27141ca474a2b6deb3ccccd2bf3cb7dfa8a2a8
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-03-25 18:28:35 -0700

    Added the comments previously mentioned to Day 3

--- 2015/Day 3/Part 1/code.js
+++ 2015/Day 3/Part 1/code.js
@@ -1,9 +1,14 @@
+// Importing the input data
 const fs = require('fs');
 const chars = fs.readFileSync('./2015/Day 3/input.txt').toString();
+// Declaring vars
 const houses = new Set();
+  // Adding the initial house to the array
 houses.add('0,0');
 let coords = [0, 0];
+// Looping over directions
 for (const char of chars) {
+  // Adjusting coordinates
   switch (char) {
     case '<':
       coords[0]--;
@@ -17,9 +22,9 @@ for (const char of chars) {
     case 'v':
       coords[1]--;
       break;
-
   }
-  console.log(char);
+  // Adds the house to the set
   houses.add(coords[0] + ',' + coords[1]);
 }
+// Print the answer
 console.log(houses.size);
