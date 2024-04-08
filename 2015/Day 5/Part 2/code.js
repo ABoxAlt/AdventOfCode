d09e74a5872fe04248219e90d8ac8504689341fd
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-04-07 19:49:17 -0700

    Finished Day 5

--- 2015/Day 5/Part 2/code.js
+++ 2015/Day 5/Part 2/code.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const contents = fs.readFileSync('./2015/Day 5/input.txt').toString().split('\n');
 let total = 0;
 for (const line of contents) {
-if (/([a-z])\1/i.test(line) && !/ab|cd|pq|xy/i.test(line) && /(a|e|i|o|u).*(a|e|i|o|u).*(a|e|i|o|u)/.test(line)) {
+if (/([a-z][a-z]).*\1/i.test(line) && /([a-z]).\1/i.test(line)) {
     total ++;
   }
 }
